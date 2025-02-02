fa6de133cde1edd486fdbee617c44e1aa779d286
majordomo-admin <admin@majordomo.systems>
2025-02-02 13:09:24 -0500

    Initial Commit;

--- db/seed.ts
+++ db/seed.ts
@@ -5,19 +5,19 @@ export default async function seed() {
     {
       id: 1,
       name: "Ubuntu Desktop",
-      url: "github:majordomo-systems/nix-flake-generator/flakes/system/ubuntu/desktop/base_flake.nix",
+      url: "github:majordomo-studio/nix-flake-generator/flakes/system/ubuntu/desktop/base_flake.nix",
       type: "os"
 	  },
 	{
       id: 2,
       name: "Ubuntu Server",
-      url: "github:majordomo-systems/nix-flake-generator/flakes/system/ubuntu/server/base_flake.nix",
+      url: "github:majordomo-studio/nix-flake-generator/flakes/system/ubuntu/server/base_flake.nix",
       type: "os"
     },
     {
       id: 3,
       name: "MacOS",
-      url: "github:majordomo-systems/nix-flake-generator/flakes/system/macos/base_flake.nix",
+      url: "github:majordomo-studio/nix-flake-generator/flakes/system/macos/base_flake.nix",
       type: "os"
     },
     // If you have your dev environment flakes in your repository,
@@ -25,37 +25,37 @@ export default async function seed() {
     {
       id: 4,
       name: "Data Engineering",
-      url: "github:majordomo-systems/nix-flake-generator/flakes/database/base_dataengineering_flake.nix",
+      url: "github:majordomo-studio/nix-flake-generator/flakes/database/base_dataengineering_flake.nix",
       type: "dev"
 	},
     {
       id: 5,
       name: "Django",
-      url: "github:majordomo-systems/nix-flake-generator/flakes/development/base_django_flake.nix",
+      url: "github:majordomo-studio/nix-flake-generator/flakes/development/base_django_flake.nix",
       type: "dev"
 	},
 	{
       id: 6,
       name: "Go",
-      url: "github:majordomo-systems/nix-flake-generator/flakes/development/base_go_flake.nix",
+      url: "github:majordomo-studio/nix-flake-generator/flakes/development/base_go_flake.nix",
       type: "dev"
     },
     {
       id: 7,
       name: "Javascript",
-      url: "github:majordomo-systems/nix-flake-generator/flakes/development/base_javascript_flake.nix",
+      url: "github:majordomo-studio/nix-flake-generator/flakes/development/base_javascript_flake.nix",
       type: "dev"
     },
 	{
       id: 8,
       name: "Network Security",
-      url: "github:majordomo-systems/nix-flake-generator/flakes/networking/base_network_flake.nix",
+      url: "github:majordomo-studio/nix-flake-generator/flakes/networking/base_network_flake.nix",
       type: "dev"
 	},
 	{
       id: 9,
       name: "Python",
-      url: "github:majordomo-systems/nix-flake-generator/flakes/development/base_python_flake.nix",
+      url: "github:majordomo-studio/nix-flake-generator/flakes/development/base_python_flake.nix",
       type: "dev"
     },
   ]);
